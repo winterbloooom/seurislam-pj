a6c54f6970a30c8ead0383d8f1c34a04f813c524
EunGi Han <717lumos@gmail.com>
2022-06-16 15:13:20 +0900

    editing unittest.cpp

--- executables/unittest.cpp
+++ executables/unittest.cpp
@@ -2,6 +2,16 @@
 // Created by 717lumos on 22. 6. 16.
 //
 #include <gtest/gtest.h>
+#include "system/slam_assembly.h"
+
+class CheckoutTests : public ::testing::Test {
+public:
+
+protected:
+  Checkout checkOut;
+};
+
+TEST()
 
 TEST(Test_Name_Major, Test_Name_Minor)
 {
